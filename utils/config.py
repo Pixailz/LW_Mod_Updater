96d7a187a324cdb6fe4cc6954ebc97c8960f53ca
Pixailz <Pixailz1@gmail.com>
2025-02-25 00:46:44 +0100

    add automatic finding of the Logic World folder

--- utils/config.py
+++ utils/config.py
@@ -2,10 +2,45 @@
 
 import os
 
-if os.name == "posix":
-	MODZ_FOLDER = "/home/pix/.local/share/Steam/steamapps/common/Logic World/GameData"
-elif os.name == "nt":
-	MODZ_FOLDER = "Z:\Jeux\SteamLibrary\steamapps\common\Logic World\GameData"
+def find_folder(folder, root="/"):
+	TO_AVOID = [
+		"$WINDOWS.~BT",
+		"$Recycle.Bin"
+		"dev",
+		"tmp",
+		"run"
+		"var"
+	]
+
+	try:
+		for path, dirs, files in os.walk(root, topdown=True):
+			dirs[:] = [d for d in dirs if d not in TO_AVOID]
+
+			if folder in [ os.path.basename(d) for d in dirs ]:
+				return os.path.join(path, folder)
+
+	except PermissionError as e:
+		print(e)
+	return None
+
+MODZ_FOLDER_PATH = os.path.abspath(
+	os.path.join(__file__, os.pardir, "FOUNDED_MODZ_FOLDER")
+)
+
+if os.path.isfile(MODZ_FOLDER_PATH):
+	with open(MODZ_FOLDER_PATH, "r") as f:
+		MODZ_FOLDER = f.read()
+else:
+	print("Searching for the 'Logic World' main folder")
+	MODZ_FOLDER = find_folder("Logic World")
+	if MODZ_FOLDER is None:
+		print("Error 'Logic World' folder could not be found")
+		exit(1)
+	print(f"Found at {MODZ_FOLDER}")
+	with open(MODZ_FOLDER_PATH, "w") as f:
+		f.write(MODZ_FOLDER)
+
+MODZ_FOLDER += "GameData"
 
 MODZ = {
 	"Ecconia's Main repo": {	# repo_name
