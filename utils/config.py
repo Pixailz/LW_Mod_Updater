2593c8f66b17dd8ccb9e2af0e47302a8c9d6d3da
Pixailz <Pixailz1@gmail.com>
2025-03-20 14:31:23 +0100

    - reorganise config.py - research for folder if not found - add .config folder to FIND_AVOID_POSIX

--- utils/config.py
+++ utils/config.py
@@ -2,6 +2,65 @@
 
 import os
 
+MODZ = {
+	"Ecconia's Main repo": {	# repo_name
+		"repo": (
+			"master",
+			"https://github.com/Ecconia/Ecconia-LogicWorld-Mods.git"
+		),
+		"modz": [
+			"HarmonyForLogicWorld",			# mandatory for gui thing
+			"AssemblyLoader",				#
+			"EccsGuiBuilder",				#
+			"EccsLogicWorldAPI",			#
+
+			"WireTracer",					# extra
+			"CustomWirePlacer",				#
+			"RandomDebugCollection",		#
+			"DisableCollision",				#
+
+			"EcconiaCPUServerComponents",	# extra ++
+			"EcconiasChaosClientMod",		#
+		]
+	},
+	"GHXX's Main repo": {
+		"repo": (
+			"master",
+			"https://github.com/GHXX/LogicWorld-TcpBridge"
+		),
+		"modz": [
+			"Tcp Bridge"
+		]
+	},
+	"CheeseModUtils": {
+		"repo": (
+			"master",
+			"https://github.com/cheese3660/CheeseUtilMod"
+		),
+		"modz": [
+			"CheeseRamMenu",
+			"CheeseUtilMod"
+		]
+	}
+}
+# where the cache are stored
+GIT_CACH_DIR = ".cache"
+# Rename or not succ to jecs files
+RENAME_SUCC_2_JECS = True
+
+# LOG
+## padding for the level
+LOG_PAD_STR = "   "
+## set loging level
+### -2 disable
+### -1 all
+### from here N contain M < N
+### 0 main title
+### 1 some info
+### 2 finded manifest.succ, local | remote commit id from cache
+### 3 succ info
+LOG_DEB_LVL = 2
+
 def get_nt_drives():
 	drives = [f"{l}:\\" for l in 'CDEFGHIJKLMNOPQRSTUVWXYZ']
 	for drive in drives:
@@ -24,7 +83,8 @@ FIND_AVOID_POSIX = [
 	"tmp",
 	"run",
 	"var",
-	"proc"
+	"proc",
+	".config"
 ]
 
 
@@ -59,11 +119,11 @@ MODZ_FOLDER_PATH = os.path.abspath(
 	os.path.join(__file__, os.pardir, "FOUNDED_MODZ_FOLDER")
 )
 
-if os.path.isfile(MODZ_FOLDER_PATH):
-	with open(MODZ_FOLDER_PATH, "r") as f:
-		MODZ_FOLDER = f.read()
-else:
+def search_lw_folder():
 	print("Searching for the 'Logic World' main folder")
+
+	global MODZ_FOLDER
+
 	MODZ_FOLDER = find_folder("Logic World")
 	if MODZ_FOLDER is None:
 		print("Error 'Logic World' folder could not be found")
@@ -72,53 +132,13 @@ else:
 	with open(MODZ_FOLDER_PATH, "w") as f:
 		f.write(MODZ_FOLDER)
 
+
+if os.path.isfile(MODZ_FOLDER_PATH):
+	with open(MODZ_FOLDER_PATH, "r") as f:
+		MODZ_FOLDER = f.read()
+	if not os.path.isdir(MODZ_FOLDER):
+		search_lw_folder()
+else:
+	search_lw_folder()
+
 MODZ_FOLDER = os.path.join(MODZ_FOLDER, "GameData")
-
-MODZ = {
-	"Ecconia's Main repo": {	# repo_name
-		"repo": (
-			"master",
-			"https://github.com/Ecconia/Ecconia-LogicWorld-Mods.git"
-		),
-		"modz": [
-			"HarmonyForLogicWorld",			# mandatory for gui thing
-			"AssemblyLoader",				#
-			"EccsGuiBuilder",				#
-			"EccsLogicWorldAPI",			#
-
-			"WireTracer",					# extra
-			"CustomWirePlacer",				#
-			"RandomDebugCollection",		#
-			"DisableCollision",				#
-
-			"EcconiaCPUServerComponents",	# extra ++
-			"EcconiasChaosClientMod",		#
-		]
-	},
-	"GHXX's Main repo": {
-		"repo": (
-			"master",
-			"https://github.com/GHXX/LogicWorld-TcpBridge"
-		),
-		"modz": [
-			"Tcp Bridge"
-		]
-	}
-}
-# where the cache are stored
-GIT_CACH_DIR = ".cache"
-# Rename or not succ to jecs files
-RENAME_SUCC_2_JECS = True
-
-# LOG
-## padding for the level
-LOG_PAD_STR = "   "
-## set loging level
-### -2 disable
-### -1 all
-### from here N contain M < N
-### 0 main title
-### 1 some info
-### 2 finded manifest.succ, local | remote commit id from cache
-### 3 succ info
-LOG_DEB_LVL = 2
